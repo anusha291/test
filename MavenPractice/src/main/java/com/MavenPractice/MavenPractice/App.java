867bf2eff3c5844283cb7d11e8a6824fa6a43eaa
AN20169354 <AN20169354@10.218.32.63>
2021-03-04 16:24:35 +0530

    MavenPractice commit

---
+++ MavenPractice/src/main/java/com/MavenPractice/MavenPractice/App.java
@@ -0,0 +1,13 @@
+package com.MavenPractice.MavenPractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
